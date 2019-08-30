64b009a65076fcf9f7f0007e96baddb20b9dc923
KKambi <37494867+KKambi@users.noreply.github.com>
2019-08-30 18:47:53 +0900

    interest 박스크기 조절하다가 중지

--- interest.js
+++ interest.js
@@ -1,10 +1,23 @@
 const interest = {
 
     init(){
-        const tagNode = document.querySelector("#interest")
-        tagNode.addEventListener("keypress", (pressEvent) => {
+        const txtareaNode = document.querySelector("#interest")
+        txtareaNode.addEventListener("keydown", (pressEvent) => {
             const inputCharacter = pressEvent.key;
             
+            //줄이 변경될 때마다 15px씩 늘리면 됨
+            let text = txtareaNode.value;
+            let previousHeight = txtareaNode.offsetHeight;
+            let newDom = document.createElement('textarea');
+            newDom.setAttribute('name', 'interest');
+            newDom.setAttribute('id', 'interest');
+            newDom.setAttribute('class', 'txtarea');
+            //실제 코드에는 상위돔에 집어넣어주세요.
+        
+            // var inputWidth =  $('#virtual_dom').width() + 10; // 글자 하나의 대략적인 크기 
+        
+            // $(input).css('width', inputWidth); 
+            // $('#virtual_dom').remove();
         })
     }
 }
