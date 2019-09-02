442a072e6d41c6b5802add3adf659f8e59d9b9b9
KKambi <37494867+KKambi@users.noreply.github.com>
2019-09-02 22:33:11 +0900

    가입하기 시 발생하는 오류 모달 구현

--- todo_join_button.js
+++ todo_join_button.js
@@ -35,22 +35,21 @@ const joinButton = {
     init: function(){
         this.preventSubmit()
         this.addCheckEvent()
+        this.addCloseEvent()
     },
+
     preventSubmit: function(){
         let joinButton = document.querySelector("#join-button")
         joinButton.addEventListener("click", (clickEvent) => {
             clickEvent.preventDefault()
         })
     },
+
     addCheckEvent: function(){
         let joinButton = document.querySelector("#join-button")
         joinButton.addEventListener("click", function(){
             this.checkEmptyInput()
         }.bind(this))
-    },
-    openModal: function(){
-        let errorDiv = document.querySelector(".join__content h2").nextSibling
-        console.log(errorDiv)
     },
     checkEmptyInput: function(){
         let errorList = []
@@ -65,17 +64,49 @@ const joinButton = {
 
         //에러가 있다면 모달을 띄운다.
         if (errorList.length > 0){
+            this.fillDiv(errorList)
             this.openModal(errorList)
         }
-        /*
-        가입하기 버튼을 클릭했을 때, 모든 항목이 입력된 상태라면 
-        모든 내용이 비동기로 서버로 보내지고 결과를 받은 후 자동로그인이 된다.
-        자동로그인이 된 상태로 메인화면으로 이동된다.
-        */
         else{
-
+            /*
+            가입하기 버튼을 클릭했을 때, 모든 항목이 입력된 상태라면 
+            모든 내용이 비동기로 서버로 보내지고 결과를 받은 후 자동로그인이 된다.
+            자동로그인이 된 상태로 메인화면으로 이동된다.
+            */
         }
     },
+
+    openModal: function(){
+        let modal = document.querySelector(".join__overlay").parentNode
+        modal.setAttribute("class", "modal")
+    },
+    closeModal: function(){
+        let modal = document.querySelector(".join__overlay").parentNode
+        modal.setAttribute("class", "modal hidden")
+    },
+
+    fillDiv: function(errorList){
+        let errorDiv = document.querySelector(".join__content h2").nextElementSibling
+        errorList.forEach((error) => {
+            let newDiv = document.createElement("div")
+            let newText = document.createTextNode(error)
+            newDiv.appendChild(newText)
+
+            errorDiv.appendChild(newDiv)
+        })
+    },
+    clearDiv: function(){
+        let errorDiv = document.querySelector(".join__content h2").nextElementSibling
+        errorDiv.innerHTML = ""
+    },
+
+    addCloseEvent: function(){
+        let confirmButton = document.querySelector(".join__agree")
+        confirmButton.addEventListener("click", function(){
+            this.closeModal()
+            this.clearDiv()
+        }.bind(this))
+    }
 }
 
 initializationButton.init()
