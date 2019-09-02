a202ef40a070936878e8e70d52d9c6a2a038f2b3
KKambi <37494867+KKambi@users.noreply.github.com>
2019-09-02 19:56:19 +0900

    초기화/취소 모달과 버튼 구현

--- todo_join_button.js
+++ todo_join_button.js
@@ -1,11 +1,35 @@
 
 const button = {
     init: function(){
+        this.preventSubmit()
+        this.addOpenModalEvent()
+        this.addInitializationEvent()
+        this.addCancleEvent()
+    },
+    addOpenModalEvent: function(){
+        document.querySelector("#initialization-button").addEventListener("click", () => {
+            document.querySelector(".init__overlay").parentNode.setAttribute("class", "modal")
+        })
+    },
+    preventSubmit: function(){
         let buttonNodeList = document.querySelectorAll(".primary-button")
         buttonNodeList.forEach((node) => {
             node.addEventListener("click", (clickEvent) => {
                 clickEvent.preventDefault()
             })
+        })
+    },
+    addInitializationEvent: function(){
+        let initalizationButton = document.querySelector(".init__agree")
+        initalizationButton.addEventListener("click", () => {
+            document.querySelector(".join_form").reset()
+            document.querySelector(".init__overlay").parentNode.setAttribute("class", "modal hidden")
+        })
+    },
+    addCancleEvent: function(){
+        let cancelButton = document.querySelector(".init__cancel")
+        cancelButton.addEventListener("click", () => {
+            document.querySelector(".init__overlay").parentNode.setAttribute("class", "modal hidden")
         })
     }
 }
