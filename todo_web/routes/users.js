45168647ac146e88d519ad56b2e4482f15a4ee5f
KKambi <37494867+KKambi@users.noreply.github.com>
2019-09-05 12:59:46 +0900

    Fix css href error in todo_login.pug

--- todo_web/routes/users.js
+++ todo_web/routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/join', function(req, res, next) {
+  res.render('todo_join');
 });
+
+router.get('/login', function(req, res, next){
+  res.render('todo_login')
+})
 
 module.exports = router;
